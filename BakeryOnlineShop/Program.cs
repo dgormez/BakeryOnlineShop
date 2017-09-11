f296cad52b16b328f4e4e12f21ae9ebf01a47ebe
dgormez <dgormez@gmail.com>
2017-09-11 16:09:27 +0200

    First commit

--- BakeryOnlineShop/Program.cs
+++ BakeryOnlineShop/Program.cs
@@ -11,6 +11,7 @@ namespace BakeryOnlineShop
     {
         public static void Main(string[] args)
         {
+            //This is my first web app made with VS2017
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
